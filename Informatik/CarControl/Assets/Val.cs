7a0ea76ca3ce1425c7060f3c0b6c8540fd8f9b74
matthiasGmayer <Matthias.Georg.Mayer@Gmail.com>
2020-10-21 12:18:14 +0200

    c

---
+++ Informatik/CarControl/Assets/Val.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Val : MonoBehaviour, IEndDragHandler
+{
+    private Slider me;
+
+    void Awake()
+    {
+        me = gameObject.GetComponent<Slider>();
+    }
+
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        me.value = 0f;
+    }
+}
+
